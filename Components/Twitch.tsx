67cd2b603b6274db606309319f39450e42a47653
osean-jim <119638097+osean-jim@users.noreply.github.com>
2023-12-06 20:02:55 +0900

    fasfasf

--- Components/Twitch.tsx
+++ Components/Twitch.tsx
@@ -4,8 +4,6 @@ import ReactTwitchEmbedVideo from "react-twitch-embed-video";
 export default function Twitch({ channel} : {channel: string}) {
 
   return (
-      <>
         <ReactTwitchEmbedVideo channel={channel} height="100%" width="100%" />
-      </>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
-import Chat from "@/app/Components/Chat";
-import Twitch from "@/app/Components/Twitch";
+import dynamic from "next/dynamic";
+
+const Chat = dynamic(() => import("../Components/Chat"), { ssr: false });
+const Twitch = dynamic(() => import("../Components/Twitch"), { ssr: false });
 import { useCookies } from "react-cookie";
 import * as React from "react";
 
@@ -30,17 +32,27 @@ export default function Home() {
             <button onClick={() => {
               removeCookie('mhjoa')
             }}>Remove Cookie</button>
-          </div> : <div>
-            <select>
-              <option value="kick">KICK</option>
-              <option value="twitch">TWITCH</option>
+          </div> : <div className="grid grid-cols-2">
+            <div>
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">플랫폼</label>
+            <select  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+              <option value="twitch">Twitch</option>
+              <option value="kick">Kick</option>
             </select>
-            <input
-                value={selectedChannel}
-                onChange={(event) => {
-                  setSelectedChannel(event.target.value)}
-            } />
-            <button onClick={handleSubmit}>Submit</button>
+            </div>
+            <div>
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                채널명
+              </label>
+              <input
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  value={selectedChannel}
+                  onChange={(event) => {
+                    setSelectedChannel(event.target.value)}
+                  } />
+            </div>
+
+            <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleSubmit}>확인</button>
           </div>}
         </div>
         <div className="bg-[#ff0000]"><Chat /></div>
